f1d96c7747542403a2e412aa768c03cb718aaf21
Paradox456 <islamannafi@gmail.com>
2025-12-18 19:48:18 -0500

    Fixed and made layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Navbar } from "src/components/layout/navbar";
-import { Footer } from "src/components/layout/footer";
-
-
+import { Navbar } from "../components/layout/navbar";
+import { Footer } from "../components/layout/footer";
+import { ThemeProvider } from "../components/layout/theme-provider";
 
 export const metadata: Metadata = {
   title: "Echelon Dashboard | AI-Powered Productivity",
